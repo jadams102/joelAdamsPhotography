b5ca7adeda19a3f285fcc3a536eea26eace8cc32
Joel Adams <joeladamsdesign@gmail.com>
2020-02-04 20:19:31 -0800

    fix slideshow

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,41 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log(jquery('span'))
+    var imagePath = '';
+    if(window.innerWidth > 768) {
+      imagePath = './assets/img/slides/';
+    } else {
+      imagePath = './assets/img/slides/mobile/mobile-';
+    }
+    var bg = jquery("div.bg");
+    var array = Array.from({length: 20}, (x,i) => i);
+    function getRandomIntInclusive(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+    }
+    var count = 20;
+
+
+    setInterval(function() {
+      if (count <= 1) {
+        array = Array.from({length: 20}, (x,i) => i);
+        count= 20
+      }
+      var int = getRandomIntInclusive(1,count);
+      var image = array[int] + 1;
+      console.log(array);
+      console.log(int);
+      array.splice(int, 1);
+      count--;
+      if(jquery(bg[0]).hasClass("active")) {
+        jquery(bg[1]).css("background-image", "url(" + imagePath + "img-" + image + ".jpg" +")")
+        jquery(bg[0]).fadeOut(1500).removeClass("active");
+      } else {
+        jquery(bg[0]).css("background-image", "url(" + imagePath + "img-" + image + ".jpg" +")").fadeIn(1500).addClass("active")
+        jquery(bg[1]).removeClass("active");
+      }
+    }, 3000)
   }
 
 }
