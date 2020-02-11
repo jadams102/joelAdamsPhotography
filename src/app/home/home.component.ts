e7cadc017f4fe7a109bca9a7a9bd309857df2e5d
Joel Adams <joeladamsdesign@gmail.com>
2020-02-10 18:37:05 -0800

    add controls to slideshow

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,29 +18,89 @@ export class HomeComponent implements OnInit {
       imagePath = './assets/img/slides/mobile/mobile-';
     }
     var bg = jquery("div.bg");
-    var array = Array.from({length: 20}, (x,i) => i);
-    function getRandomIntInclusive(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+    var imgNumber = 2;
+    var changing = false;
+
+    function isChanging() {
+      changing = !changing
     }
 
+    jquery("div.ctrl-left").on("click", function () {
+      if(!changing) {
+        clearInterval(interval);
+        changing = true;
+        if(imgNumber === 1) {
+          imgNumber = 20;
+        } else {
+          imgNumber--;
+        }
+        if(jquery(bg[0]).hasClass("active")) {
+          jquery(bg[1]).css("background-image", "url(" + imagePath + "img-" + imgNumber + "-min.jpg" +")")
+          jquery(bg[0]).fadeOut(600).removeClass("active");
+          setTimeout(function() {
+            isChanging();
+          }, 600)
+        } else {
+          setTimeout(function() {
+            isChanging();
+          }, 600)
+          jquery(bg[0]).css("background-image", "url(" + imagePath + "img-" + imgNumber + "-min.jpg" +")").fadeIn(600).addClass("active")
+          jquery(bg[1]).removeClass("active");
+        }
+      }
+    })
 
-    setInterval(function() {
-      if (array.length <= 1) {
-        array = Array.from({length: 20}, (x,i) => i);
+    jquery("div.ctrl-right").on("click", function () {
+      if(!changing) {
+        clearInterval(interval);
+        changing = true;
+        if(imgNumber === 20) {
+          imgNumber = 1;
+        } else {
+          imgNumber++;
+        }
+        if(jquery(bg[0]).hasClass("active")) {
+          jquery(bg[1]).css("background-image", "url(" + imagePath + "img-" + imgNumber + "-min.jpg" +")")
+          jquery(bg[0]).fadeOut(600).removeClass("active");
+          setTimeout(function() {
+            isChanging();
+          }, 600)
+        } else {
+          setTimeout(function() {
+            isChanging();
+          }, 600)
+          jquery(bg[0]).css("background-image", "url(" + imagePath + "img-" + imgNumber + "-min.jpg" +")").fadeIn(600).addClass("active")
+          jquery(bg[1]).removeClass("active");
+        }
       }
-      var int = getRandomIntInclusive(1,array.length);
-      var image = array[int] + 1;
-      array.splice(int, 1);
-      if(jquery(bg[0]).hasClass("active")) {
-        jquery(bg[1]).css("background-image", "url(" + imagePath + "img-" + image + "-min.jpg" +")")
-        jquery(bg[0]).fadeOut(2000).removeClass("active");
-      } else {
-        jquery(bg[0]).css("background-image", "url(" + imagePath + "img-" + image + "-min.jpg" +")").fadeIn(2000).addClass("active")
-        jquery(bg[1]).removeClass("active");
+    })
+
+    var interval = setInterval(function() {
+      if (!changing) {
+        changing = true;
+        if (imgNumber === 20) {
+          imgNumber = 1;
+        }
+        if(jquery(bg[0]).hasClass("active")) {
+          jquery(bg[1]).css("background-image", "url(" + imagePath + "img-" + imgNumber + "-min.jpg" +")")
+          jquery(bg[0]).fadeOut(2000).removeClass("active");
+          setTimeout(function() {
+            isChanging();
+          }, 2000)
+        } else {
+          setTimeout(function() {
+            isChanging();
+          }, 2000)
+          jquery(bg[0]).css("background-image", "url(" + imagePath + "img-" + imgNumber + "-min.jpg" +")").fadeIn(2000).addClass("active")
+          jquery(bg[1]).removeClass("active");
+        }
+        imgNumber++;
       }
-    }, 8000)
+
+
+
+    }, 7000)
+
   }
 
 }
