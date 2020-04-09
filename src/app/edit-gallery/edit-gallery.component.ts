f7767057aa9e1f318d0463ea0fd48d1bb7325c5d
Joel Adams <joeladamsdesign@gmail.com>
2020-04-09 14:15:30 -0700

    add edit gallery form

--- src/app/edit-gallery/edit-gallery.component.ts
+++ src/app/edit-gallery/edit-gallery.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { GalleryService } from '../services/gallery.service';
+import { Gallery } from '../models/gallery.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-gallery',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-gallery.component.scss']
 })
 export class EditGalleryComponent implements OnInit {
+  currentGallery: Gallery;
+  errorText: string;
 
-  constructor() { }
+  constructor(private galleryService: GalleryService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.galleryService.getGalleries().subscribe((data)=> {
+      for(let i = 0; i < data.length; i++) {
+        if(data[i].title === this.route.snapshot.params.name) {
+          this.currentGallery = data[i];
+        }
+      }
+    });
   }
 
+  updateGallery(gallery) {
+    this.galleryService.updateGallery(gallery);
+  }
 }
